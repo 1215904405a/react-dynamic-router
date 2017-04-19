96577043eb7c1f624ae63944cf3ce569f578d7c0
1215904405a <1215904405@qq.com>
2017-04-19 09:50:30 +0800

    Create App.jsx
    项目入口

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+/* create by yongwang */
+import React, { Component, PropTypes } from 'react';
+import ReactDOM, { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import route from './Config/Route'; //路由配置
+import store from './Config/Store';
+
+import './Style/common'; //加载公共样式
+import './Style/tip';
+import './Style/index';
+
+store.subscribe(function() {
+  // console.log(store.getState());
+});
+let App = document.createElement('div')
+App.className = "appwrap"
+render(
+  <Provider store={store}>
+        {route}
+    </Provider>,
+  document.body.appendChild(App)
+);
+
+process.env.NODE_ENV !== 'production' && module.hot.accept();
